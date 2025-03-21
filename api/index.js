c837a60a8ad61ec3414fd329c51dc3772204449f
jimmyg1927 <jimmy@streamz.store>
2025-03-21 12:50:37 +0000

    Switch to zero-config deployment

--- api/index.js
+++ api/index.js
@@ -17,6 +17,19 @@ app.use((req, res, next) => {
   next();
 });
 
+// Add CORS headers
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+// Handle OPTIONS requests
+app.options('*', (req, res) => {
+  res.status(200).end();
+});
+
 // Subscription limits
 const SUBSCRIPTION_LIMITS = {
   demo: 10,
