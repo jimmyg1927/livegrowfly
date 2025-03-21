93d4f31b3c893cba782ca695028f45444852a6c5
jimmyg1927 <jimmy@streamz.store>
2025-03-21 13:44:52 +0000

    Fix authentication and deployment configuration

--- api/index.js
+++ api/index.js
@@ -9,35 +9,7 @@ const app = express();
 const prisma = new PrismaClient();
 
 app.use(express.json());
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  next();
-});
 
-// Add CORS headers
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
-
-// Handle OPTIONS requests
-app.options('*', (req, res) => {
-  res.status(200).end();
-});
-
-// Subscription limits
-const SUBSCRIPTION_LIMITS = {
-  demo: 10,
-  personal: 200,
-  entrepreneur: 650,
-  business: 4000
-};
-
-// Auth middleware
 const authenticateUser = async (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
