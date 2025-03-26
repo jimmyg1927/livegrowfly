0842e8dee301488eee8acccd585230f62aab9d92
jimmyg1927 <jimmy@streamz.store>
2025-03-26 12:19:39 +0000

    Fix Shopify OAuth router export

--- shopify/shopifyAuth.js
+++ shopify/shopifyAuth.js
@@ -2,7 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import pkg from '@shopify/shopify-api';
 
-const { shopifyApi, ApiVersion } = pkg;
+const { shopifyApi, ApiVersion, session } = pkg;
 
 dotenv.config();
 
@@ -21,7 +21,7 @@ const shopify = shopifyApi({
   hostName: (SHOPIFY_APP_URL || '').replace(/^https?:\/\//, ''),
   isEmbeddedApp: true,
   apiVersion: ApiVersion.October23,
-  sessionStorage: new pkg.session.MemorySessionStorage(),
+  sessionStorage: new session.MemorySessionStorage(),
 });
 
 const router = express.Router();
@@ -65,4 +65,3 @@ router.get('/auth/callback', async (req, res) => {
 });
 
 export default router;
-
