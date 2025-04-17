f12d27acb62f1fe375c21b76db5a971ab33c05ec
jimmyg1927 <jimmy@streamz.store>
2025-04-17 18:02:14 +0000

    🐛 Fix: added default export to signup page

---
+++ app/signup/page.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export default function SignUpPage() {
+  return (
+    <div className="flex items-center justify-center h-screen text-2xl text-white">
+      Signup Page Placeholder
+    </div>
+  );
+}
