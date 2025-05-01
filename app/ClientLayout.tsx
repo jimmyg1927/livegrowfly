bc037c4db1b4995297e00954bff3f60bd066bdea
jimmyg1927 <jimmy@streamz.store>
2025-05-01 16:53:48 +0000

    Fix: Added missing 'name' prop to Header component in ClientLayout

--- app/ClientLayout.tsx
+++ app/ClientLayout.tsx
@@ -1,17 +1,17 @@
-'use client'
+'use client';
 
-import React from 'react'
-import Sidebar from '../src/components/Sidebar'
-import Header from '../src/components/Header'
+import React from 'react';
+import Sidebar from '../src/components/Sidebar';
+import Header from '../src/components/Header';
 
 export default function ClientLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen bg-background text-textPrimary">
       <Sidebar />
       <div className="flex-1 flex flex-col">
-        <Header />
+        <Header name="Growfly User" />
         <main className="flex-1 overflow-y-auto p-6">{children}</main>
       </div>
     </div>
-  )
+  );
 }
