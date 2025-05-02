d2ef76de4a311d8cab990a4ebca0691d6628a076
jimmyg1927 <jimmy@streamz.store>
2025-05-02 11:05:29 +0000

    ✅ Fix Editor component prop types and EditorBubbleMenu path

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -6,13 +6,12 @@ import StarterKit from '@tiptap/starter-kit'
 import Underline from '@tiptap/extension-underline'
 import Link from '@tiptap/extension-link'
 import TextAlign from '@tiptap/extension-text-align'
-import EditorBubbleMenu from '../../components/EditorBubbleMenu'
-
+import EditorBubbleMenu from './EditorBubbleMenu'
 
 interface Props {
   content: string
-  setContent?: (val: string) => void
-  onChange?: (val: string) => void
+  setContent: (val: string) => void
+  onChange?: (val: string) => void // Optional external change handler
 }
 
 export default function Editor({ content, setContent, onChange }: Props) {
@@ -24,17 +23,17 @@ export default function Editor({ content, setContent, onChange }: Props) {
       TextAlign.configure({ types: ['heading', 'paragraph'] }),
     ],
     content,
-    onUpdate: ({ editor }) => {
+    onUpdate({ editor }) {
       const newContent = editor.getHTML()
-      setContent?.(newContent)
-      onChange?.(newContent)
+      setContent(newContent)
+      if (onChange) onChange(newContent)
     },
   })
 
   return (
-    <div className="relative">
+    <div className="border rounded-lg">
       {editor && <EditorBubbleMenu editor={editor} />}
-      <EditorContent editor={editor} className="prose dark:prose-invert min-h-[200px] bg-background border border-card rounded-lg p-4" />
+      <EditorContent editor={editor} className="min-h-[200px] p-4 outline-none" />
     </div>
   )
 }
