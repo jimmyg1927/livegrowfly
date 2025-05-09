2f137747949e448004ae6ca6f8c0ff7e2a03de13
jimmyg1927 <jimmy@streamz.store>
2025-05-09 14:49:12 +0000

    ♻️ Remove layout from /register

--- app/register/layout.tsx
+++ app/register/layout.tsx
@@ -1,7 +1,7 @@
 export default function RegisterLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="bg-[#0f60ff] text-white">{children}</body>
     </html>
   );
 }
